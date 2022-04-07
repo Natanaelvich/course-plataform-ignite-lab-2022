d70fce1cf3f2a4ade9710c6f02923170d561d469
Natanael Lima <taelima1997@gmail.com>
2022-04-07 00:01:35 -0300

    add imports database and add controller test

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from 'src/database/database.module';
+import { TestController } from './test.controller';
 
-@Module({})
+@Module({
+  imports: [ConfigModule.forRoot(), DatabaseModule],
+  controllers: [TestController],
+})
 export class HttpModule {}
