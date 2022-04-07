9fa4d64088c4e0fc00ce773d83d0a47c2615d7df
Natanael Lima <taelima1997@gmail.com>
2022-04-06 23:29:09 -0300

    add redirects serverside

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,9 +1,26 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>HELLO WORLD</h1>
+import { getSession } from "@auth0/nextjs-auth0";
+import { GetServerSideProps } from "next";
 
-      <a href="/api/auth/login">Login</a>
-    </div>
-  );
+export default function Home() {
+  return null;
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = getSession(req, res);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/api/auth/login",
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      redirect: {
+        destination: "/app",
+        permanent: false,
+      },
+    };
+  }
+};
