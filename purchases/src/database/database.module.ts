4c58005057000601d41836f9bfeaddc9cbf04906
Natanael Lima <taelima1997@gmail.com>
2022-04-07 00:01:12 -0300

    add prisma service databse

--- purchases/src/database/database.module.ts
+++ purchases/src/database/database.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class DatabaseModule {}
