a8a4a130f1c0a95c59c5ce3a4aa932f2ad2c0cbd
Natanael Lima <taelima1997@gmail.com>
2022-04-07 11:32:46 -0300

    update structure to support graphql and add authorization

---
+++ classroom/src/http/http.module.ts
@@ -0,0 +1,20 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import path from 'path';
+import { DatabaseModule } from 'src/database/database.module';
+import { TestResolver } from './test.resolver';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  providers: [TestResolver],
+})
+export class HttpModule {}
