540823320f80b18d3c0c1a341272260e432a7548
Natanael Lima <taelima1997@gmail.com>
2022-04-08 11:17:34 -0300

    add model gql Purchase

---
+++ purchases/src/http/graphql/models/purchase.ts
@@ -0,0 +1,30 @@
+import { Field, ID, ObjectType, registerEnumType } from '@nestjs/graphql';
+import { Product } from './product';
+
+enum PurchaseStatus {
+  PENDING = 'PENDING',
+  APPROVED = 'APPROVED',
+  FAILED = 'FAILED',
+}
+
+registerEnumType(PurchaseStatus, {
+  name: 'PuchaseStatus',
+  description: 'Available purchase statuses',
+});
+
+@ObjectType()
+export class Purchase {
+  @Field(() => ID)
+  id: string;
+
+  @Field(() => PurchaseStatus)
+  status: PurchaseStatus;
+
+  @Field(() => Date)
+  created_at: Date;
+
+  @Field(() => Product)
+  product: Product;
+
+  product_id: string;
+}
