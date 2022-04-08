a6809366b6f611b42a45060d179923707d89f53c
Natanael Lima <taelima1997@gmail.com>
2022-04-08 11:17:42 -0300

    add PurchasesService

---
+++ purchases/src/services/purchases.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+
+@Injectable()
+export class PurchasesService {
+  constructor(private prisma: PrismaService) {}
+  listAllPurchases() {
+    return this.prisma.purchase.findMany({
+      orderBy: { created_at: 'desc' },
+    });
+  }
+}
