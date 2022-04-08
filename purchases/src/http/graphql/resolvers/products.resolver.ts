962d7e0a543e29dc16f70e398ca35b504d276aa3
Natanael Lima <taelima1997@gmail.com>
2022-04-08 10:40:08 -0300

    add ProductsResolver

---
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -0,0 +1,22 @@
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { ProductsService } from 'src/services/products.service';
+import { CreateProductInput } from '../inputs/create-product-input';
+import { Product } from '../models/product';
+
+@Resolver('products')
+export class ProductsResolver {
+  constructor(private productsService: ProductsService) {}
+
+  @Query(() => [Product])
+  products() {
+    return this.productsService.listAllProducts();
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @Mutation(() => Product)
+  createProduct(@Args('data') data: CreateProductInput) {
+    return this.productsService.createProduct(data);
+  }
+}
