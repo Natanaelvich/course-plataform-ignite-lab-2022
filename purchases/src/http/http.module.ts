f0891f4b2db71dc1a849e5b3767eecbe3404d216
Natanael Lima <taelima1997@gmail.com>
2022-04-08 22:46:46 -0300

    add CustomersService to http module

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import path from 'path';
 import { DatabaseModule } from 'src/database/database.module';
+import { CustomersService } from 'src/services/customers.service';
 import { ProductsService } from 'src/services/products.service';
 import { PurchasesService } from 'src/services/purchases.service';
 import { ProductsResolver } from './graphql/resolvers/products.resolver';
@@ -19,10 +20,14 @@ import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
     }),
   ],
   providers: [
+    //resolvers
     ProductsResolver,
-    ProductsService,
     PurchasesResolver,
+
+    //services
+    ProductsService,
     PurchasesService,
+    CustomersService,
   ],
 })
 export class HttpModule {}
