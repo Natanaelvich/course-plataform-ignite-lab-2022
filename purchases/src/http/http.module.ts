eb9a469b3832494dffd1238c6da66e53ed68b523
Natanael Lima <taelima1997@gmail.com>
2022-04-08 23:12:35 -0300

    add CustomersResolver to http module

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -7,6 +7,7 @@ import { DatabaseModule } from 'src/database/database.module';
 import { CustomersService } from 'src/services/customers.service';
 import { ProductsService } from 'src/services/products.service';
 import { PurchasesService } from 'src/services/purchases.service';
+import { CustomersResolver } from './graphql/resolvers/customer.resolver';
 import { ProductsResolver } from './graphql/resolvers/products.resolver';
 import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
 
@@ -23,6 +24,7 @@ import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
     //resolvers
     ProductsResolver,
     PurchasesResolver,
+    CustomersResolver,
 
     //services
     ProductsService,
