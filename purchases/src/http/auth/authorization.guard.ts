6be26250927e783a37c165e6e5d91227e254e87c
Natanael Lima <taelima1997@gmail.com>
2022-04-08 23:12:24 -0300

    remove log unused

--- purchases/src/http/auth/authorization.guard.ts
+++ purchases/src/http/auth/authorization.guard.ts
@@ -42,7 +42,6 @@ export class AuthorizationGuard implements CanActivate {
 
       return true;
     } catch (error) {
-      console.log({ error });
       throw new UnauthorizedException(error);
     }
     return true;
--- purchases/src/http/auth/current-user.ts
+++ purchases/src/http/auth/current-user.ts
@@ -10,8 +10,6 @@ export const CurrentUser = createParamDecorator(
     const ctx = GqlExecutionContext.create(context);
     const req = ctx.getContext().req;
 
-    console.log(req.user);
-
     return req.user;
   },
 );
