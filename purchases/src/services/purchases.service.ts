72cdd96e8f9400637d32197e5680a7bc2c0237b9
Natanael Lima <taelima1997@gmail.com>
2022-04-08 22:46:05 -0300

    add createPurchase to service

--- purchases/src/services/purchases.service.ts
+++ purchases/src/services/purchases.service.ts
@@ -1,12 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 
+interface CreatePurchaseParams {
+  customer_id: string;
+  product_id: string;
+}
 @Injectable()
 export class PurchasesService {
   constructor(private prisma: PrismaService) {}
   listAllPurchases() {
     return this.prisma.purchase.findMany({
       orderBy: { created_at: 'desc' },
+    });
+  }
+
+  async createPurchase({ customer_id, product_id }: CreatePurchaseParams) {
+    const product = await this.prisma.product.findUnique({
+      where: {
+        id: product_id,
+      },
+    });
+
+    if (!product) {
+      throw new Error('Product not found');
+    }
+
+    return await this.prisma.purchase.create({
+      data: {
+        customer_id,
+        product_id,
+      },
     });
   }
 }
