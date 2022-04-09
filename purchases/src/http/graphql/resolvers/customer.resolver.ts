fd58acd1fd57d288c0ccffbda1bbff1808d6c75b
Natanael Lima <taelima1997@gmail.com>
2022-04-08 23:11:21 -0300

    add CustomersResolver

---
+++ purchases/src/http/graphql/resolvers/customer.resolver.ts
@@ -0,0 +1,27 @@
+import { UseGuards } from '@nestjs/common';
+import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { AuthUser, CurrentUser } from 'src/http/auth/current-user';
+import { CustomersService } from 'src/services/customers.service';
+import { PurchasesService } from 'src/services/purchases.service';
+import { Customer } from '../models/customer';
+import { Purchase } from '../models/purchase';
+
+@Resolver(() => Customer)
+export class CustomersResolver {
+  constructor(
+    private customersService: CustomersService,
+    private purchasesService: PurchasesService,
+  ) {}
+
+  @Query(() => Customer)
+  @UseGuards(AuthorizationGuard)
+  me(@CurrentUser() user: AuthUser) {
+    return this.customersService.getCustomersByAuthUserId(user.sub);
+  }
+
+  @ResolveField(() => [Purchase])
+  purchases(@Parent() customer: Customer) {
+    return this.purchasesService.getPurchasesByCustomerId(customer.id);
+  }
+}
