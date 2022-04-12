2729e497e7c3e6005cc546a77e50d915ae32db80
Natanael Lima <taelima1997@gmail.com>
2022-04-12 14:40:29 -0300

    create enrollment on purchase.new-purchase event

--- classroom/src/messaging/controllers/purchases.controller.ts
+++ classroom/src/messaging/controllers/purchases.controller.ts
@@ -1,5 +1,8 @@
 import { Controller } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
+import { CoursesService } from 'src/services/courses.service';
+import { EnrollmentsService } from 'src/services/enrollments.service';
+import { StudentsService } from 'src/services/student.service';
 
 export interface Custumer {
   authUserId: string;
@@ -18,8 +21,25 @@ export interface PurchaseCreated {
 
 @Controller()
 export class PurchaseController {
+  constructor(
+    private studentsService: StudentsService,
+    private enrollmentsService: EnrollmentsService,
+    private coursesService: CoursesService,
+  ) {}
+
   @EventPattern('purchase.new-purchase')
   async purchaseCreated(@Payload('value') payload: PurchaseCreated) {
-    console.log('test', payload);
+    const student = await this.studentsService.findOrCreate({
+      auth_user_id: payload.custumer.authUserId,
+    });
+
+    const course = await this.coursesService.findOrCreate(
+      payload.product.title,
+    );
+
+    await this.enrollmentsService.createEnrollment({
+      student_id: student.id,
+      course_id: course.id,
+    });
   }
 }
