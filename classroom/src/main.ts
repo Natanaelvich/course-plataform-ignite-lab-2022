4907ba121a0a5c8d1ced1fcb27da5f1a1af38f72
Natanael Lima <taelima1997@gmail.com>
2022-04-11 23:58:45 -0300

    add config connectMicroservice

--- classroom/src/main.ts
+++ classroom/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'classroom',
+        brokers: ['localhost:29092'],
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+
   await app.listen(3334);
 }
 bootstrap();
