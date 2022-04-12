9bc770d93fc917fe88192cbb8a298cafb8fc0f76
Natanael Lima <taelima1997@gmail.com>
2022-04-12 14:40:49 -0300

    add providers and imports to MessagingModule

--- classroom/src/messaging/messaging.module.ts
+++ classroom/src/messaging/messaging.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database/database.module';
+import { CoursesService } from 'src/services/courses.service';
+import { EnrollmentsService } from 'src/services/enrollments.service';
+import { StudentsService } from 'src/services/student.service';
 import { PurchaseController } from './controllers/purchases.controller';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [PurchaseController],
+  providers: [StudentsService, EnrollmentsService, CoursesService],
 })
 export class MessagingModule {}
