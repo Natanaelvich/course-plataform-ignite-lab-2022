193986613dfafbaac6d2597e2e71596bedf79cf6
Natanael Lima <taelima1997@gmail.com>
2022-04-12 23:42:54 -0300

    add config gatway

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,8 +1,24 @@
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        cors: true,
+      },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'purchases', url: 'http://localhost:3333/graphql' },
+            { name: 'classroom', url: 'http://localhost:3334/graphql' },
+          ],
+        }),
+      },
+    }),
+  ],
 })
 export class AppModule {}
