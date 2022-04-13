a42a8a602306ab40fd3cdc37760934e2dc1551f0
Natanael Lima <taelima1997@gmail.com>
2022-04-13 13:10:39 -0300

    add initial config apollo client

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,10 +1,15 @@
+import { ApolloProvider } from "@apollo/client";
 import { UserProvider } from "@auth0/nextjs-auth0";
+import { apolloClient } from "../lib/apollo";
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
+        <ApolloProvider client={apolloClient}>
+
       <Component {...pageProps} />
+        </ApolloProvider>
     </UserProvider>
   );
 }
--- web/src/pages/app.tsx
+++ web/src/pages/app.tsx
@@ -1,13 +1,23 @@
+import { gql, useQuery } from "@apollo/client";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import React from "react";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 
-const pages: React.FC = () => {
+const TEST = gql`
+  query GetProducts {
+    products {
+      title
+      slug
+    }
+  }
+`;
+const Home: React.FC = () => {
+  const { data } = useQuery(TEST);
   return (
     <div>
       <Header />
-
+      <pre>{JSON.stringify(data, null, 3)}</pre>
       <Footer />
     </div>
   );
@@ -15,4 +25,4 @@ const pages: React.FC = () => {
 
 export const getServerSideProps = withPageAuthRequired();
 
-export default pages;
+export default Home;
