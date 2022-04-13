af400463bf77923fc70cb8fbd8741ec48c94d911
Natanael Lima <taelima1997@gmail.com>
2022-04-13 15:08:17 -0300

    remove ApolloProvider

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,15 +1,10 @@
-import { ApolloProvider } from "@apollo/client";
 import { UserProvider } from "@auth0/nextjs-auth0";
-import { apolloClient } from "../lib/apollo";
-import '../styles/global.css'
+import "../styles/global.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
-        <ApolloProvider client={apolloClient}>
-
       <Component {...pageProps} />
-        </ApolloProvider>
     </UserProvider>
   );
 }
